193ebc322edd05e25d0cd46dfb39159ccae07d42
GuDoYoon <rhehfl418q@gmail.com>
2024-06-03 10:37:53 +0900

    asdfasdf

---
+++ codingtest_gdy/programouse2.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    let answer = '';
+    if(num%2==0)
+        {
+            answer="Even";
+        }
+    else
+        {
+            answer="Odd";
+        }
+    return answer;
+}
