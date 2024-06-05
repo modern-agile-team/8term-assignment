c272f91f5e148e70bb0573e6ddec6404337950ce
전희나 <jeonheena@jeonhuinaui-MacBookPro.local>
2024-06-05 19:37:29 +0900

    20240605(2)

---
+++ codingtest_JHN/수박수박수.js
@@ -0,0 +1,13 @@
+//수박수박수박수박수
+function solution(n) {
+    let answer = "";
+    for(let i=1; i<=n; i++){ 
+        if(i%2 === 0) {
+            answer += "박";
+        }
+        else {
+            answer += "수";
+        }
+        }
+    return answer;
+}
