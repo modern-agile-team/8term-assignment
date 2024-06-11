f21ce6bd21c607fb8d42ece9d75856a17f2a2698
class <hororror1@gmail.com>
2024-06-11 19:44:16 +0900

    4일차 과제풀이

--- Hong/문자열 내 p와 y의 개수 세기.js
+++ Hong/문자열 내 p와 y의 개수 세기.js
@@ -1,11 +1,11 @@
 //과제3
 function solution(s) {
-  var answer = true;
+  let answer = true;
   let count1 = 0;
   let count2 = 0;
   for (let i = 0; i < s.length; i++) {
-    if (s[i] == "p" || s[i] == "P") count1++;
-    if (s[i] == "y" || s[i] == "Y") count2++;
+    if (s[i] === "p" || s[i] === "P") count1++;
+    if (s[i] === "y" || s[i] === "Y") count2++;
   }
   if (count1 != count2) answer = false;
 
--- Hong/서울에서 김서방 찾기.js
+++ Hong/서울에서 김서방 찾기.js
@@ -3,7 +3,7 @@ function solution(seoul) {
   var answer = "";
 
   for (let i = 0; i < 1000; i++) {
-    if (seoul[i] == "Kim") {
+    if (seoul[i] === "Kim") {
       answer = "김서방은 " + i + "에 있다";
       break;
     }
