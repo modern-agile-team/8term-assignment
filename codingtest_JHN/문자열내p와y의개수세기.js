4dc23a414a8498316a5442beea5fcd9e34d139af
전희나 <jeonheena@jeonhuinaui-MacBookPro.local>
2024-06-12 18:34:32 +0900

    문자열내림차순으로배치하기

--- codingtest_JHN/문자열내p와y의개수세기.js
+++ codingtest_JHN/문자열내p와y의개수세기.js
@@ -1,21 +1,25 @@
 //문자열 내 p와 y의 개수 세기
+const str = "asdfasfsssssss";
 function solution(str) {
   let num_P = 0;
   let num_Y = 0;
+  console.log(str, 1);
   //let : 가변, const : 불변
-  for(let i = 0; i < str.length; i++) {
-    str.split('');
-    if(str[i] === 'p' || str[i] === 'P'){
+  for (let i = 0; i < str.length; i++) {
+    str.split("");
+    if (str[i] === "p" || str[i] === "P") {
       num_P++;
     }
-    if(str[i] === 'y' || str[i] === 'Y'){
+    if (str[i] === "y" || str[i] === "Y") {
       num_Y++;
     }
-}
-  if(num_P === num_Y){
-    return true;
   }
-  else {
+  console.log(str);
+  if (num_P === num_Y) {
+    return true;
+  } else {
     return false;
   }
 }
+
+solution(str);
---
+++ codingtest_JHN/문자열내림차순으로배치하기.js
@@ -0,0 +1,16 @@
+/*const b = [3,2,4,5,1];//내림차순
+
+const des = b.sort((x,y)=>(y - x));
+console.log(des);
+*/
+const s = "Zbcdefg";
+function solution(s) {
+  const arr = s.split(""); //문자열을 나눠서 각각 분석
+  arr.sort(function (a, b) {
+    if (a < b) return 1;
+    if (a > b) return -1;
+    if (a === b) return 0;
+  });
+  return arr.join("");
+}
+console.log(solution(s));
