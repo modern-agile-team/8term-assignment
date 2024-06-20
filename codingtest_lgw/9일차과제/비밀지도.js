a8c45f292433ddea2a88c713b60d8e7af1e1ee38
gwgw123 <yja20850@naver.com>
2024-06-20 14:34:13 +0900

    9일차 주석 수정했습니다

--- codingtest_lgw/9일차과제/비밀지도.js
+++ codingtest_lgw/9일차과제/비밀지도.js
@@ -4,11 +4,13 @@ function solution(n, arr1, arr2) {
   let newarr2 = [];
 
   for (let i = 0; i < n; i++) {
+    //이진수로 변경 -> newarr 없이 이진수로 변경만 해도 되었을까요?
     newarr1[i] = arr1[i].toString(2);
     newarr2[i] = arr2[i].toString(2);
   }
 
   for (let j = 0; j < n; j++) {
+    // 0을 넣어줌으로써 변의 길이를 n으로 맞춘다.
     for (let i = 0; i < n; i++) {
       if (newarr1[i].length < n) {
         newarr1[i] = "0" + newarr1[i];
@@ -18,10 +20,11 @@ function solution(n, arr1, arr2) {
       }
     }
   }
-  console.log(newarr1);
-  console.log(newarr2);
+  console.log(newarr1); // 확인용출력
+  console.log(newarr2); // 확인용출력
 
   for (let j = 0; j < n; j++) {
+    // 두 지도 겹쳤을때 둘 다 1이라면 # 삽입, 아니면 공백
     for (let i = 0; i < n; i++) {
       if (newarr1[j][i] === "1" || newarr2[j][i] === "1") {
         answer[j] += "#";
