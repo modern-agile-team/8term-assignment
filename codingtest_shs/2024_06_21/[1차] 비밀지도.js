26276c388457eec4742b070a87e12c41f7e60d2e
bluetree7878 <bluetree7878@naver.com>
2024-06-24 14:33:22 +0900

    6월 24일 코딩테스트 1문제 (키패드 누르기) 풀었습니다.

--- codingtest_shs/2024_06_21/[1차] 비밀지도.js
+++ codingtest_shs/2024_06_21/[1차] 비밀지도.js
@@ -1,11 +1,10 @@
 function solution(n, arr1, arr2) {
   let answer = [];
+  let arr = []; // 2진수 담을 배열
 
   for (let i = 0; i < n; i++) {
     // arr1, arr2의 배열을 2진수로 변환
     const binary_num = (arr1[i] | arr2[i]).toString(2);
-
-    let arr = []; // 2진수 담을 배열
 
     // 2진수 배열의 길이에 n을 빼준 값을 2진수 배열의 길이만큼 순회
     for (let j = binary_num.length - n; j < binary_num.length; j++) {
@@ -18,5 +17,7 @@ function solution(n, arr1, arr2) {
     // arr 배열의 원소들을 모두 문자열로 바꿔주어 붙여줌
     answer.push(arr.join(""));
   }
-  return answer;
+  return console.log(answer);
 }
+
+solution(5, [1, 2, 3, 4, 5], [6, 7, 8, 9, 10]);
