d7ecae27e60537ea7590276effcacd564e6fb997
GuDoYoon <rhehfl418q@gmail.com>
2024-06-28 11:03:03 +0900

    폴더 구조 최적화

--- todolist/app/app.js
+++ todolist/app/app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use("/", home); //루트로 오면 홈으로 보내주는 미들웨어임
+app.use("/", home); //루트로 오면 홈으로 보 내주는 미들웨어임
 module.exports = app;
