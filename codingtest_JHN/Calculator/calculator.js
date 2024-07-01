ef6e98e116d05c57ecbff7aaee37d7e3983af6c5
전희나 <jeonheena@jeonhuinaui-MacBookPro.local>
2024-07-01 17:59:41 +0900

    계산기

--- codingtest_JHN/Calculator/calculator.js
+++ codingtest_JHN/Calculator/calculator.js
@@ -2,4 +2,16 @@
 // 입력버튼 누를 시 결과부분에 결과값 나오는 함수
 
 alert("입력되었습니다.");
+
+// 값을 받아내는 작업
+// 판별하는 작업
+// 값에 따라 연산하는 작업
+
 function inputBtn() {}
+function result() {
+    document.getElementById("  ").innerHTML = "  ";
+}
+//exit 입력 시 result는 없고 종료 경고창 출력
+function exit() {
+    if ()
+}
