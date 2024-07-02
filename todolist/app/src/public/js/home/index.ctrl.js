a67d9678e0eb48ee8ceb6f4689c5dd4f4660a4ac
GuDoYoon <rhehfl418q@gmail.com>
2024-07-02 18:04:59 +0900

    CRUD 기능 submit 안하고 생성되게 구현중

--- todolist/app/src/public/js/home/index.ctrl.js
+++ todolist/app/src/public/js/home/index.ctrl.js
@@ -1,51 +1,38 @@
-"use strict";
+// "use strict";
 
-document.addEventListener("DOMContentLoaded", loadList);
+// function printList(data) {
+//   const todolist = document.querySelector("#todolist");
 
-function loadList() {
-  fetch("load", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      if (res.success) {
-        printList(res.data);
-      }
-    });
-}
+//   for (let i = 0; i < data.length; i++) {
+//     let tmpCheck = data[i].is_check ? "" : "checked";
+//     let tmpText = data[i].is_check ? "" : "Cancellation-line";
 
-function printList(data) {
-  const todolist = document.querySelector("#todolist");
+//     todolist.innerHTML += ` <sapn>
+//                                 <input type="checkbox" id="${data[i].id}" onClick="updateCheck(this)"
+//                                 ${tmpCheck}/>
 
-  for (let i = 0; i < data.length; i++) {
-    let tmpCheck = data[i].is_check ? "" : "checked";
-    // let tmpText = data[i].is_check
-    //   ? `${data[i].description}`
-    //   : `줄긋기 --${data[i].description}`;
-    todolist.innerHTML += ` 
-                            <td>
-                                <input type="checkbox" id="${data[i].id}" onClick="updateCheck(this)"
-                                ${tmpCheck}/>
-                                <input type = "text" id = "text${data[i].id}" value = "${data[i].description}" disabled></>
-                                <a id="${data[i].id}" name ="update" onClick ="editMod(this)">[수정]</a>
-                                <a id="${data[i].id}" name="delete" onClick="deleteList(this.id)">
-                                [삭제]</a>
-                                <hr/>
-                            </td>
-                            `;
-  }
-}
-function editMod(val) {
-  const inputText = document.querySelector(`#text${val.id}`);
-  inputText.removeAttribute("disabled"); //입력가능하게 만들기
-  val.setAttribute("onClick", `updateText(getValue(${val.id}))`); //수정버튼의 onClick이벤트를 readMod함수로 연결
-  val.innerText = "확인";
-}
-function getValue(val) {
-  const id = val;
-  const value = document.querySelector(`#text${id}`).value;
-  return { id: id, value: value };
-}
+//                                <input type = "text" id = "text${data[i].id}" value = "${data[i].description}" class ="${tmpText} input1 list-text" disabled></>
+//                                 <button type="button" id="${data[i].id}" name ="update" onClick ="editMod(this)">수정</button>
+//                                 <button type ="button" id="${data[i].id}" name="delete" onClick="deleteList(this.id)">
+//                                 삭제</button>
+//                                 <hr class ="underline"/>
+//                             </sapn>
+//                             `;
+//   }
+// }
+// function editMod(val) {
+//   const inputText = document.querySelector(`#text${val.id}`);
+//   inputText.removeAttribute("disabled"); //입력가능하게 만들기
+//   val.setAttribute("onClick", `updateText(getValue(${val.id}))`); //수정버튼의 onClick이벤트를 readMod함수로 연결
+//   val.innerText = "확인";
+// }
+// function getValue(val) {
+//   const id = val;
+//   const value = document.querySelector(`#text${id}`).value;
+//   return { id: id, value: value };
+// }
+// module.exports = {
+//   printList,
+//   editMod,
+//   getValue,
+// };
--- todolist/app/src/public/js/home/index.js
+++ todolist/app/src/public/js/home/index.js
@@ -1,4 +1,58 @@
 "use strict";
+let maxId = 0;
+document.addEventListener("DOMContentLoaded", loadList);
+function loadList() {
+  fetch("load", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        printList(res.data);
+      }
+    });
+}
+
+function printList(data) {
+  const todolist = document.querySelector("#todolist");
+
+  for (let i = 0; i < data.length; i++) {
+    let tmpCheck = data[i].is_check ? "" : "checked";
+    let tmpText = data[i].is_check ? "" : "Cancellation-line";
+
+    todolist.innerHTML += ` <sapn>
+                                <input type="checkbox" id="${data[i].id}" onClick="updateCheck(this)"
+                                ${tmpCheck}/>
+                               
+                               <input type = "text" id = "text${data[i].id}" value = "${data[i].description}" class ="${tmpText} input1 list-text" disabled></>
+                                <button type="button" id="${data[i].id}" name ="update" onClick ="editMod(this)">수정</button>
+                                <button type ="button" id="${data[i].id}" name="delete" onClick="deleteList(this.id)">
+                                삭제</button>
+                                <hr class ="underline"/>
+                            </sapn>
+                            `;
+  }
+}
+function getMaxId(data) {
+  console.log(data[data.length - 1].id);
+  if (data.length === 0) return 0;
+  return data[data.length - 1].id;
+}
+function editMod(val) {
+  const inputText = document.querySelector(`#text${val.id}`);
+  inputText.removeAttribute("disabled"); //입력가능하게 만들기
+  val.setAttribute("onClick", `updateText(getValue(${val.id}))`); //수정버튼의 onClick이벤트를 readMod함수로 연결
+  val.innerText = "확인";
+}
+function getValue(val) {
+  const id = val;
+  const value = document.querySelector(`#text${id}`).value;
+  return { id: id, value: value };
+}
+//----------------------------------------------------------------------------------------------------------
 const addBtn = document.querySelector("#add"),
   list = document.querySelector("#list"),
   text = document.querySelector("#text");
@@ -13,7 +67,6 @@ text.addEventListener("keypress", (event) => {
 });
 
 function updateText(idText) {
-  console.log(idText);
   const req = {
     id: idText.id,
     col: "description",
@@ -84,6 +137,7 @@ function addList() {
   const req = {
     text: text.value,
   };
+
   fetch("/", {
     method: "POST",
     headers: {
@@ -94,7 +148,7 @@ function addList() {
     .then((res) => res.json())
     .then((res) => {
       if (res.success) {
-        list.submit();
+        //list.submit();
       } else {
         return alert(res.msg);
       }
