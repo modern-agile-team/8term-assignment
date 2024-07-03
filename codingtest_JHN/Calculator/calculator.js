23fcb40d746fdb1138fd35646e5910e6a57bfe08
전희나 <jeonheena@jeonhuinaui-MacBookPro.local>
2024-07-03 17:35:06 +0900

    계산기_3일차

--- codingtest_JHN/Calculator/calculator.js
+++ codingtest_JHN/Calculator/calculator.js
@@ -1,53 +1,92 @@
-//0. 입력 -> 계산한다 -> 결과나온다.
-//1. 연산자와 피연산자를 나눈다....
-//2. 자바스크립트를 나가는 함수
-//3. 곱셈과 나눗셈을 먼저 찾는다 ( 배열화 )
-//4. 계산을 한다 -> 함수 4개
+let inputVal; //전역변수
+let arrDel;
 
-//값을 받아서 공백없애기 함수까지 하여 입력하는 함수
+//1. 값을 받아서, 공백없애기 함수까지 입력하는 함수
 function getVal() {
-  let inputVal = document.getElementById("inputVal").value;
-  testVal(inputVal);
+  inputVal = document.getElementById("inputVal").value; //입력받은 값 변수 inputVal
+  arrDel = testVal(inputVal);
+  myFunction(finalCal());
 }
 
-//공백없애기 함수
-function testVal(x) {
-  console.log(x.replace(/\s/g, ""));
-  const testing = x.replace(/\s/g, "");
-  myFunction(testing);
+// 입력값에 공백 없애기 + 그 값을 배열화하기
+function testVal(s) {
+  let delSpace = s.replace(/\s/g, "");
+  return [...delSpace];
 }
 
+//2. 연산자가 없어질 때까지의 함수
+function finalCal() {
+  while (arrDel.length === 1) {
+    ///
+    priority();
+  }
+  return; //마지막값
+}
+//3. 연산자 우선순위 뽑는(곱셈, 나눗셈) 함수 - 그 다음 덧셈, 뺄셈
+function priority() {
+  for (let i = 0; i < arrDel.length; i++) {
+    if (arrDel[i] === "*" || arrDel[i] === "/") {
+      calMulDiv(i);
+      return;
+    } else if (arrDel[i] === "+" || arrDel[i] === "-") {
+      calAddSub(i);
+      return;
+    }
+  }
+}
+//4. 곱셈, 나눗셈 연산자 나왔을 때
+function calMulDiv(i) {
+  let answer = 0;
+  if (arrDel[i] === "*") {
+    answer = multi(arrDel[i - 1], arrDel[i + 1]);
+  } else {
+    answer = division(arrDel[i - 1], arrDel[i + 1]);
+  }
+  arrDel.splice(i - 1, 3, answer);
+}
+
+//5. 덧셈, 뺄셈 연산자 나왔을 때
+function calAddSub(i) {
+  let answer = 0;
+  if (arrDel[i] === "+") {
+    answer = add(arrDel[i - 1], arrDel[i + 1]);
+  } else {
+    answer = subtract(arrDel[i - 1], arrDel[i + 1]);
+  }
+  arrDel.splice(i - 1, 3, answer);
+}
+
+//6.  result 나오게 하는 함수
 function myFunction(s) {
   let result = document.getElementById("result");
-  result.innerHTML = s;
+  result.innerHTML = s.toFixed(2);
 }
 
-// function test(v) {
-//     const expression = v.replace(/^[0-9\+\*\/\-]/, "");
-//     return expression;
-//   //연산자 피연산자 구분
-// }
+//+. 계산구현
+function add(a, b) {
+  return a + b;
+}
 
-//2. 계산구현
-// function add() {
+function subtract(a, b) {
+  return a - b;
+}
 
-// }
+function multi(a, b) {
+  return a * b;
+}
 
-// function subtract{
+function division(a, b) {
+  return a / b;
+}
 
-// }
+//소수 둘째자리까지 구현하기
 
-// function multi{
-
-// }
-
-// function division{
-
-// }
-
-//exit 입력 시 종료 경고창 출력 함수
+//7. exit 입력 시 종료 경고창 출력 함수
 function end() {
   if (inputVal === "exit") {
     alert("종료");
+  } else {
+    //최종결과화면출력
+    getVal();
   }
 }
