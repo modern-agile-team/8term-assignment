a224232a3cfdc2fa88fb21f9a1e211a65df04a92
bluetree7878 <bluetree7878@naver.com>
2024-07-05 14:23:45 +0900

    src 폴더를 만들어서 안에 routes, views폴더로 정리했습니다.

--- todolist_shs/app/app.js
+++ todolist_shs/app/app.js
@@ -7,4 +7,13 @@ dotenv.config();
 
 const app = express();
 
+// 라우팅
+const home = require('./src/routes/home');
+
+// 앱 세팅
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/', home);
+
 module.exports = app;
