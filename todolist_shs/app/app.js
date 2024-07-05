0e79ce057922bcc9bb56e466c310891770955d80
bluetree7878 <bluetree7878@naver.com>
2024-07-05 16:39:44 +0900

    ejs, css, js 파일을 연결하였습니다.

--- todolist_shs/app/app.js
+++ todolist_shs/app/app.js
@@ -13,6 +13,7 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use('/', home);
 
--- todolist_shs/app/src/routes/home/index.js
+++ todolist_shs/app/src/routes/home/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home/index');
+  res.render('home/todolist');
 });
 
 module.exports = router;
