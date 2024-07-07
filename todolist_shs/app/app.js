dc33a75ec3368486efe415997e88e4b7a5e0ee80
bluetree7878 <bluetree7878@naver.com>
2024-07-07 13:23:32 +0900

    MySQL 연결 및 라우트, 미들웨어 설정 완료

--- todolist_shs/app/app.js
+++ todolist_shs/app/app.js
@@ -2,10 +2,14 @@
 
 // 모듈
 const express = require('express');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+
+// 미들웨어 설정
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // 라우팅
 const home = require('./src/routes/home');
--- todolist_shs/app/src/routes/home/index.js
+++ todolist_shs/app/src/routes/home/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const db = require('../../config/db');
 
 router.get('/', (req, res) => {
   res.render('home/todolist');
