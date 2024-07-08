fa7f7aca395920eb695bbbc3b91e34482004885e
bluetree7878 <bluetree7878@naver.com>
2024-07-08 11:54:43 +0900

    아이콘 hover기능, Enter or 플러스 버튼 클릭했을 때 이벤트 발생 함수 만들었습니다.

--- todolist_shs/app/src/public/js/home/todolist.js
+++ todolist_shs/app/src/public/js/home/todolist.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('test');
+const todo = document.querySelector('#input'),
+  plusButton = document.querySelector('#plus-button');
+
+// 플러스버튼 클릭시 add 함수 호출
+plusButton.addEventListener('click', add);
+
+// enter키를 눌렀을 때 add 함수 호출
+function enter() {
+  if (window.event.keyCode == 13) return add();
+}
+
+// add 함수가 잘 작동되는지 테스트(미완)
+function add() {
+  if (todo.value) {
+    return alert(todo.value);
+  } else {
+    addErr();
+  }
+}
+
+// 입력란에 입력한 값이 없을 때
+function addErr() {
+  if (!todo.value) {
+    return alert('할 일을 입력해주십시오.');
+  }
+}
