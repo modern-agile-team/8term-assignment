445f5f07e9ce2a998bae522080f4512be2af8cec
bluetree7878 <bluetree7878@naver.com>
2024-07-10 06:01:05 +0900

    체크박스 div 요소 하나만 적용되는 오류 수정 및 todo 수정 함수, todo 저장함수 완성하였습니다.

--- todolist_shs/app/src/public/js/home/todolist.js
+++ todolist_shs/app/src/public/js/home/todolist.js
@@ -5,6 +5,7 @@ const todo = document.querySelector('#input'),
   todoList = document.querySelector('#todo-list'),
   box = document.querySelector('#box');
 
+document.addEventListener('keyup', enter);
 let todoId = 1;
 
 const addId = () => {
@@ -15,11 +16,16 @@ const addId = () => {
 plusButton.addEventListener('click', add);
 plusButton.addEventListener('click', addId);
 
-// enter키를 눌렀을 때 add, addId 함수 호출
-function enter() {
-  if (window.event.keyCode == 13) {
-    add();
-    addId();
+// enter키를 눌렀을 때 add, addId, saveItem 함수 호출
+function enter(event) {
+  if (event.keyCode === 13) {
+    const activeElement = document.activeElement;
+    if (activeElement.id === 'input') {
+      add();
+      addId();
+    } else if (activeElement.classList.contains('editable')) {
+      saveItem(activeElement);
+    }
   }
 }
 
@@ -27,13 +33,15 @@ function enter() {
 function add() {
   if (todo.value) {
     const div = document.createElement('div');
-    div.innerHTML = `<input type="checkbox" id="complete-line" name="todo" onclick="Check(this)">  <input type="text" id="list-text" disabled value="${todo.value}">
-    <span>
-        <!-- 수정 아이콘 -->
-        <img src="https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png" id="edit" onclick="editItem(this)" />
-        <!-- 삭제 아이콘 -->
-        <img src="https://cdn-icons-png.flaticon.com/512/12/12960.png" id="delete" onclick="deleteItem(this)" />
-    </span>
+    div.innerHTML = `
+      <input type="checkbox" id="complete-line" name="todo" onclick="Check(this)">
+      <input type="text" id="list-text" disabled value="${todo.value}">
+      <span>
+          <!-- 수정 아이콘 -->
+          <img src="https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png" id="edit" onclick="editItem(this)">
+          <!-- 삭제 아이콘 -->
+          <img src="https://cdn-icons-png.flaticon.com/512/12/12960.png" id="delete" onclick="deleteItem(this)">
+      </span>
     `;
     const childDiv = todoList.appendChild(div);
     childDiv.setAttribute('id', todoId);
@@ -45,11 +53,10 @@ function add() {
 }
 
 // 체크박스 함수
-function Check() {
-  const completeLine = document.querySelector('#complete-line');
-  const listText = document.querySelector('#list-text');
+function Check(checkbox) {
+  const listText = checkbox.parentNode.querySelector('#list-text');
 
-  if (completeLine.checked) {
+  if (checkbox.checked) {
     listText.style.textDecoration = 'line-through';
     listText.style.textDecorationThickness = '2px';
     listText.style.color = 'grey';
@@ -66,12 +73,34 @@ function addErr() {
   }
 }
 
-// todo 수정 함수 (미완)
+// Todolist 수정 함수
 function editItem(element) {
-  const div = element.parentNode;
+  const div = element.parentNode.parentNode;
+  const listText = div.querySelector('#list-text');
+  if (listText.disabled) {
+    listText.disabled = false;
+    listText.classList.add('editable');
+    listText.focus();
+    element.src =
+      'https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_check-256.png'; // 수정 완료 아이콘
+  } else {
+    saveItem(listText);
+    element.src =
+      'https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png'; // 수정 아이콘
+  }
 }
 
-// todo 삭제 함수
+// Todolist 저장 함수
+function saveItem(element) {
+  const div = element.parentNode;
+  const editIcon = div.querySelector('#edit');
+  element.disabled = true;
+  element.classList.remove('editable');
+  editIcon.src =
+    'https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png'; // 수정 아이콘
+}
+
+// Todolist 삭제 함수
 function deleteItem(element) {
   const div = element.parentNode.parentNode;
   div.remove();
