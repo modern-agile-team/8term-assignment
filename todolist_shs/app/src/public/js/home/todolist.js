7cb0f6466e953d28eae65093eb7a566484eca3a6
bluetree7878 <bluetree7878@naver.com>
2024-07-09 21:44:07 +0900

    todolist div안에 고유 id를 넣었고, 공백이 있으면 문자열이 끊기는 오류를 수정하였습니다.

--- todolist_shs/app/src/public/js/home/todolist.js
+++ todolist_shs/app/src/public/js/home/todolist.js
@@ -5,27 +5,38 @@ const todo = document.querySelector('#input'),
   todoList = document.querySelector('#todo-list'),
   box = document.querySelector('#box');
 
-// 플러스버튼 클릭시 add 함수 호출
-plusButton.addEventListener('click', add);
+let todoId = 1;
 
-// enter키를 눌렀을 때 add 함수 호출
+const addId = () => {
+  todoId += 1;
+};
+
+// 플러스버튼 클릭시 add, addId 함수 호출
+plusButton.addEventListener('click', add);
+plusButton.addEventListener('click', addId);
+
+// enter키를 눌렀을 때 add, addId 함수 호출
 function enter() {
-  if (window.event.keyCode == 13) return add();
+  if (window.event.keyCode == 13) {
+    add();
+    addId();
+  }
 }
 
 // Todolist 추가 함수
 function add() {
   if (todo.value) {
     const div = document.createElement('div');
-    div.innerHTML = `<input type="checkbox" id="complete-line" name="todo" onclick="Check(this)">  <input type="text" id="list-text" disabled value=${todo.value}>
+    div.innerHTML = `<input type="checkbox" id="complete-line" name="todo" onclick="Check(this)">  <input type="text" id="list-text" disabled value="${todo.value}">
     <span>
-       <!-- 수정 아이콘 -->
+        <!-- 수정 아이콘 -->
         <img src="https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png" id="edit" onclick="editItem(this)" />
         <!-- 삭제 아이콘 -->
         <img src="https://cdn-icons-png.flaticon.com/512/12/12960.png" id="delete" onclick="deleteItem(this)" />
     </span>
     `;
-    todoList.appendChild(div);
+    const childDiv = todoList.appendChild(div);
+    childDiv.setAttribute('id', todoId);
     div.style.border = '2px solid grey';
     todo.value = '';
   } else {
@@ -55,7 +66,7 @@ function addErr() {
   }
 }
 
-// todo 수정 함수
+// todo 수정 함수 (미완)
 function editItem(element) {
   const div = element.parentNode;
 }
