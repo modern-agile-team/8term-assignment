b8dcc348d6a2cf6420a0e0a26b843e54776f61cf
bluetree7878 <bluetree7878@naver.com>
2024-07-09 14:27:14 +0900

    체크박스에 체크 돼있을 때 취소선이 생기고 글씨색을 바꾸는 함수를 추가하였습니다.

--- todolist_shs/app/src/public/js/home/todolist.js
+++ todolist_shs/app/src/public/js/home/todolist.js
@@ -13,16 +13,16 @@ function enter() {
   if (window.event.keyCode == 13) return add();
 }
 
-// add 함수가 잘 작동되는지 테스트(미완)
+// Todolist 추가 함수
 function add() {
   if (todo.value) {
     const div = document.createElement('div');
-    div.innerHTML = `${todo.value}
+    div.innerHTML = `<input type="checkbox" id="complete-line" name="todo" onclick="Check(this)">  <input type="text" id="list-text" disabled value=${todo.value}>
     <span>
        <!-- 수정 아이콘 -->
-        <img src="https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png" id="modify" onclick="editItem(this)" />
+        <img src="https://cdn0.iconfinder.com/data/icons/zondicons/20/edit-pencil-64.png" id="edit" onclick="editItem(this)" />
         <!-- 삭제 아이콘 -->
-        <img src="https://cdn-icons-png.flaticon.com/512/12/12960.png" id="elimination" onclick="deleteItem(this)" />
+        <img src="https://cdn-icons-png.flaticon.com/512/12/12960.png" id="delete" onclick="deleteItem(this)" />
     </span>
     `;
     todoList.appendChild(div);
@@ -33,11 +33,31 @@ function add() {
   }
 }
 
+// 체크박스 함수
+function Check() {
+  const completeLine = document.querySelector('#complete-line');
+  const listText = document.querySelector('#list-text');
+
+  if (completeLine.checked) {
+    listText.style.textDecoration = 'line-through';
+    listText.style.textDecorationThickness = '2px';
+    listText.style.color = 'grey';
+  } else {
+    listText.style.textDecoration = 'none';
+    listText.style.color = 'black';
+  }
+}
+
 // 입력란에 입력한 값이 없을 때
 function addErr() {
   if (!todo.value) {
     return alert('할 일을 입력해주십시오.');
   }
+}
+
+// todo 수정 함수
+function editItem(element) {
+  const div = element.parentNode;
 }
 
 // todo 삭제 함수
